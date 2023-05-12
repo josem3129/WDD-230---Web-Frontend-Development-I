aac26fee51d0189ba4a55e697dbca5a4012eb008
josem3129 <mar22069@byui.edu>
2023-05-11 21:36:37 -0600

    update 3

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -5,3 +5,78 @@ function toggleMenu(){
 const x = document.getElementById('hamburgerBtn')
 
 x.onclick = toggleMenu;
+
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay();
+let dayName = "";
+let monthName;
+
+switch(day){
+    case 0:
+        dayName = "Monday";
+        break;
+    case 1:
+        dayName = "Tuesday";
+        break;
+    case 2:
+        dayName = "Wednesday";
+        break; 
+    case 3:
+        dayName = "Thursday";
+        break;
+    case 4:
+        dayName = "Friday";
+        break;
+    case 5:
+        dayName = "Saturday";
+        break;
+    case 6:
+        dayName = "Sunday";
+        break
+}
+
+switch(month){
+    case 0:
+        monthName = "January";
+        break;
+    case 1:
+        monthName = "February";
+        break;
+    case 2:
+        monthName = "March";
+        break; 
+    case 3:
+        monthName = "April";
+        break;
+    case 4:
+        monthName = "May";
+        break;
+    case 5:
+        monthName = "June";
+        break;
+    case 6:
+        monthName = "July";
+        break
+    case 7:
+        monthName = "August";
+        break;
+    case 8:
+        monthName = "September";
+        break;
+    case 9:
+        monthName = "October";
+        break; 
+    case 10:
+        monthName = "November";
+        break;
+    case 11:
+        monthName = "Decmeber";
+        break;   
+}
+document.querySelector("#date").innerHTML= ` ${dayName} ${day}, ${monthName} ${year}` ;
+
+
+let text = document.lastModified;
+document.querySelector("#year").innerHTML = `${year}.:|:. Jose Martinez .:|:. Idaho last Updated: ${text}`;
