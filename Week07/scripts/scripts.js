6c9af8e99e04258c5acc919721371211873a07ed
josem3129 <mar22069@byui.edu>
2023-05-31 15:27:26 -0600

    week 07 update 5

--- Week07/scripts/scripts.js
+++ Week07/scripts/scripts.js
@@ -12,7 +12,7 @@ function preloadImage(img){
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px 200px 0px"
+    rootMargin: "100px"
 
 };
 
@@ -33,15 +33,15 @@ images.forEach(image => {
 
 
 
-let imagesToLoad = document.querySelectorAll("img[data-src]");
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
-  };
-};
+// let imagesToLoad = document.querySelectorAll("img[data-src]");
+// const loadImages = (image) => {
+//   image.setAttribute("src", image.getAttribute("data-src"));
+//   image.onload = () => {
+//     image.removeAttribute("data-src");
+//   };
+// };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+//   });
   
--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -88,3 +88,57 @@ if (dayName === "Saturday"){
     img.setAttribute("src", "images/DNA-website-slide_depot1.jpg")
     document.querySelector(".welcome").innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
 }
+
+
+
+ //lazy loading//
+const images = document.querySelectorAll("[data-src]")
+
+function preloadImage(img){
+    const src = img.getAttribute("data-src");
+    if(!src){
+        return;
+    }
+
+    img.src = src
+}
+
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "100px"
+
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return;
+        }else{
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target)
+        }
+    })
+}, imgOptions)
+
+images.forEach(image => {
+    imgObserver.observe(image);
+})
+
+//local storage 
+
+const countDisplay = document.querySelector("#visitedCount");
+
+let numVisit = Number(window.localStorage.getItem("visit-counter"));
+
+if(numVisit !== 0){
+    countDisplay.textContent = `Number of visits: ${numVisit}`;
+}else{
+    countDisplay.textContent = "This is your first time visiting!"
+}
+
+numVisit++;
+
+localStorage.setItem("visitedCount", numVisit);
+
+
