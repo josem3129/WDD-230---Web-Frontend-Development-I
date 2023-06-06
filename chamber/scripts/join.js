70676f0e24171abe32e7c179bcc8d1ded10197a1
josem3129 <mar22069@byui.edu>
2023-06-06 11:11:19 -0600

    week 08 update 6

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -26,6 +26,16 @@ form.addEventListener("submit", (event) => {
         event.preventDefault();
 
     }
-    
-    
+       
 })
+
+
+// // Get the modal
+// var modal = document.getElementById('table');
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
