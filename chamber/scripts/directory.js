e2b7463381aeb650bc6e42dda36162efe4c79ed3
josem3129 <mar22069@byui.edu>
2023-06-13 19:02:28 -0600

    week 09 update 7

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,14 +1,14 @@
 async function getProphetData(){
-    const response = await fetch("");
+    const response = await fetch("https://raw.githubusercontent.com/josem3129/WDD-230---Web-Frontend-Development-I/main/chamber/data.json");
     const data = await response.json();
-    prophets = data;
-    console.table(data.prophets);
-    console.table(prophets);
-    data.prophets.forEach(data =>
+    // companies = data;
+    console.table(data.companies);
+    // console.table(companies);
+    data.companies.forEach(data =>
     displayProphet(data));
 }
 
-function displayProphet(prophet){
+function displayProphet(companies){
     const cards = document.querySelector('.cards');
 
     
@@ -18,58 +18,63 @@ function displayProphet(prophet){
     let h4 = document.createElement('h4')
     let h5 = document.createElement('h5')
     let img = document.createElement('img');
-    let pBYear = prophet.birthdate;
-    let pDYear = prophet.death;
-    let bSplit = '';
-    let byear = '';
-    let dSplit = '';
-    let dyear = '';
-    if (prophet.lastname == 'Nelson'){
-        // birth year 
-            bSplit = pBYear.split(' ');
-            byear = bSplit[2]
+    // let pBYear = companies.birthdate;
+    // let pDYear = companies.death;
+    // let bSplit = '';
+    // let byear = '';
+    // let dSplit = '';
+    // let dyear = '';
+    // if (companies.lastname == 'Nelson'){
+    //     // birth year 
+    //         bSplit = pBYear.split(' ');
+    //         byear = bSplit[2]
         
-        const date = new Date();
-        // death year
-            dyear = date.getFullYear();
-    }else{
-            // birth year 
-            bSplit = pBYear.split(' ');
-            byear = bSplit[2]
+    //     const date = new Date();
+    //     // death year
+    //         dyear = date.getFullYear();
+    // }else{
+    //         // birth year 
+    //         bSplit = pBYear.split(' ');
+    //         byear = bSplit[2]
             
     
-            // death year
-            dSplit = pDYear.split(' ');
-            dyear = dSplit[2]
-    }
+    //         // death year
+    //         dSplit = pDYear.split(' ');
+    //         dyear = dSplit[2]
+    // }
 
-    if(prophet.name == 'Joseph'){
-        h2.textContent = `${prophet.name} ${prophet.lastname} - ${prophet.order}st`;
+    // if(companies.name == 'Joseph'){
+    //     h2.textContent = `${companies.name} ${companies.lastname} - ${companies.order}st`;
 
-    }else if (prophet.name == 'Brigham'){
-        h2.textContent = `${prophet.name} ${prophet.lastname} - ${prophet.order}nd`;
+    // }else if (companies.name == 'Brigham'){
+    //     h2.textContent = `${companies.name} ${companies.lastname} - ${companies.order}nd`;
                     
-    }else if(prophet.name == 'John'){
-        h2.textContent = `${prophet.name} ${prophet.lastname} - ${prophet.order}rd`;
-    }else{
-        h2.textContent = `${prophet.name} ${prophet.lastname} - ${prophet.order}th`;
-    }
-    h3.textContent = `Birthplace: ${prophet.birthplace}`
-    h4.textContent = `Birthday: ${prophet.birthdate} - Deathday${prophet.death} at Age: ${dyear - byear}`;
-    h5.textContent = `Years served - ${prophet.length}`;
+    // }else if(companies.name == 'John'){
+    //     h2.textContent = `${companies.name} ${companies.lastname} - ${companies.order}rd`;
+    // }else{
+    //     h2.textContent = `${companies.name} ${companies.lastname} - ${companies.order}th`;
+    // }
 
-    img.setAttribute('src', prophet.imageurl);
-    img.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`)
+    card.setAttribute('class', 'companyCards')
+
+    h2.textContent = `${companies.name}`
+    h3.textContent = `${companies.address}`
+    h4.textContent = `${companies.businessUrl}`;
+    h5.textContent = `Email: ${companies.email}`;
+
+    img.setAttribute('src', companies.url);
+    img.setAttribute('alt', `Portrait of ${companies.name}`)
     img.setAttribute('loading', 'lazy');
     img.setAttribute('width', '340');
     img.setAttribute('height', '440');
 
+    card.appendChild(img);
     card.appendChild(h2);
     card.appendChild(h3);
     card.appendChild(h4);
     card.appendChild(h5);
-    card.appendChild(img);
 
     cards.appendChild(card);
     
 }
+getProphetData()
