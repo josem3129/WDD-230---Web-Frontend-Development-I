4a0f318df1915d30ab6b3bc09c909856701042a0
josem3129 <mar22069@byui.edu>
2023-06-19 08:20:26 -0600

    week 07 update

---
+++ Week10/js/weatherapi.js
@@ -0,0 +1,23 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=3147dd21956cbdef375e52cb5ed3e014'
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      // displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
