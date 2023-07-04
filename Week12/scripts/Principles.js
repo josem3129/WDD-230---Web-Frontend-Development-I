b7faa7b564a0fb4d141b229589cbf3db1a8cf8be
jose Martinez <josem3129@gmail.com>
2023-07-03 21:21:11 -0600

    week 12 update 3

---
+++ Week12/scripts/Principles.js
@@ -0,0 +1,5 @@
+const date = new Date();
+const year = date.getFullYear();
+let text = document.lastModified;
+document.querySelector("#year").innerHTML = `${year}.:|:. Jose Martinez .:|:. Idaho last Updated: ${text}`;;
+
